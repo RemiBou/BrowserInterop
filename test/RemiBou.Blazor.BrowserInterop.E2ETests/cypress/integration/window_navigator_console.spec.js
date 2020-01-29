9f948d774f623320408a06553b5760c3f16ce10e
Rémi Bourgarel <remi.bourgarel@mypangee.com>
2020-01-29 10:54:51 +0100

    Fix console tests

--- test/RemiBou.Blazor.BrowserInterop.E2ETests/cypress/integration/window_navigator_console.spec.js
+++ test/RemiBou.Blazor.BrowserInterop.E2ETests/cypress/integration/window_navigator_console.spec.js
@@ -7,18 +7,13 @@ context('window.console', () => {
 
     it('Check console methods called', () => {
         cy.window()
-            .its('DotNet')
-            .should('not.be.undefined')
-            .window()
             .then((w) => {
-                w.DotNet.jsCallDispatcher.findJSFunction('console.log');
                 cy.spy(w.console, "log");
+                w.console.log.__proto__ = w.Function;
                 cy.get("#btn-console-do-test").click()
                     .then(() => {
-                        expect(w.console.log).be.called.calledTwice;
+                        expect(w.console.log).be.called.calledThrice;
                     });
-
-
             });
     });
 }
